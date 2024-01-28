08da6e364d745769fc7ee16e07ba4c4fad59d75b
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-28 11:57:25 +0800

    add operations

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
+const OPERATOR = {
+  PLUS: '+',
+  MINUS: '-',
+  MULTIPLY: '*',
+  DIVIDE: '/',
+};
 
-function operate(fistNum, operator, secondNum) {
+let firstNum;
+let secondNum;
+let operator;
 
+function operate(firstNum, operator, secondNum) {
+  switch (operator) {
+    case OPERATOR.PLUS:
+      add(firstNum, secondNum);
+    case OPERATOR.MINUS:
+      substract(firstNum, secondNum);
+    case OPERATOR.MULTIPLY:
+      multiply(firstNum, secondNum);
+    case OPERATOR.DIVIDE:
+      divide(firstNum, secondNum);
+  }
 }
 
-function add() {
-
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function substract() {
-
+function substract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply() {
-
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide() {
-  
+function divide(num1, num2) {
+  if (num2 === 0) {
+    console.log('division to zero!');
+  }
+
+  return num1 / num2;
 }
